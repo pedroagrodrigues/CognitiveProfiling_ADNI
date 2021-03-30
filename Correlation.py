7fd478572e41a80994ae41860eddf9be7465b9c4
Pedro Rodrigues <pedroagrodrigues@gmail.com>
2021-03-30 15:44:26 +0100

    started a notebook

--- Correlation.py
+++ Correlation.py
@@ -1,8 +1,13 @@
-import pandas as pd
-import numpy as np
 from Merge import Merge
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-data = Merge().data
+# Warning, ignoring some errors that will result in a NaN value of the output matrix
+
+
+path = './dataFiles/corr'
 
 
 def getCorr(x, y):
@@ -10,33 +15,50 @@ def getCorr(x, y):
     yData = []
     for i in range(len(x)):
         if x[i] != '' and y[i] != '':
-            xData.append(float(x[i]))
-            yData.append(float(y[i]))
+            xData.append(eval(str(x[i])))
+            yData.append(eval(str(y[i])))
 
     xData = np.array(xData)
     yData = np.array(yData)
-
     return np.corrcoef(xData, yData)[0, 1]
 
 
-unusedLables = ['RID', 'PTID', 'VISCODE', 'SITE', 'COLPROT', 'ORIGPROT']
+try:
+    correlations = pd.read_pickle(path+'.pickle')
+except:
+    data = Merge().data
 
-data = data.drop(columns=unusedLables)
+    unusedLables = ['RID', 'PTID', 'VISCODE', 'SITE', 'COLPROT', 'ORIGPROT']
+
+    data = data.drop(columns=unusedLables)
+
+    dataLabels = data.columns.values
+    dataNumpy = data.to_numpy()
+
+    correlations = pd.DataFrame(index=dataLabels, columns=dataLabels)
+
+    for i in range(len(dataLabels)):
+        correlations.at[dataLabels[i], dataLabels[i]] = 1
+        for j in range(i+1, len(dataLabels)):
+            corr = getCorr(dataNumpy[:, i], dataNumpy[:, j])
+            correlations.at[dataLabels[i], dataLabels[j]] = correlations.at[dataLabels[j], dataLabels[i]] = 0 if np.isnan(corr) else abs(corr)
+
+    correlations.to_pickle(path+'.pickle')
+    correlations.to_csv(path+'.csv')
+
+# correlations.fillna(0)
+# print(correlations.head(10))
+
+# sns.set_theme()
+# plt.pcolor(correlations)
+# plt.yticks(np.arange(0.5, len(correlations.index), 1), correlations.index)
+# plt.xticks(np.arange(0.5, len(correlations.columns), 1), correlations.columns)
+# plt.show()
 
 
-dataLabels = data.columns.values
-dataNumpy = data.to_numpy()
+# sns.heatmap(np.ma.filled(correlations.astype(float), np.nan), annot=True, annot_kws={"size": 7}, xticklabels=correlations.columns, yticklabels=correlations.index)
 
+# plt.show()
 
-# index = 17
-# print(f"Showing data for {dataLabels[index]}:\n")
-# print(dataNumpy[:, index])
-correlations = pd.DataFrame(index=dataLabels, columns=dataLabels)
-
-for i in range(len(dataLabels)):
-    correlations.at[dataLabels[i], dataLabels[i]] = 1
-    for j in range(i+1, len(dataLabels)):
-        correlations.at[dataLabels[i], dataLabels[j]] = correlations.at[dataLabels[j], dataLabels[i]] = getCorr(dataNumpy[:, i], dataNumpy[:, j])
-
-print(correlations.head(10))
+# print(correlations.head(10))
 # print(data.head(5))
--- Merge.py
+++ Merge.py
@@ -12,7 +12,6 @@ class Merge:
         self.data = self.loadData()
         print("Ready!")
 
-
     def loadData(self):
         try:
             data = panda.read_pickle(self.path+'.pickle')
@@ -22,7 +21,22 @@ class Merge:
 
             db = Database('ADNI', 'ADNIMERGE', credentials['user'], credentials['password'], credentials['server'])
 
-            labelsToRemove = ['_id', 'FSVERSION', 'FSVERSION_bl', 'EXAMDATE', 'EXAMDATE_bl', 'update_stamp']
+            # labelsToRemove = ['_id', 'FSVERSION', 'FSVERSION_bl', 'EXAMDATE', 'EXAMDATE_bl', 'update_stamp']
+
+            labelsToRemove = ['_id', 'EXAMDATE', 'RID', 'PTID', 'VISCODE', 'SITE', 'COLPROT', 'ORIGPROT', 'PTETHCAT', 'PTRACCAT',
+                              'PTMARRY', 'APOE4', 'FDG', 'PIB', 'AV45', 'ABETA', 'TAU', 'PTAU', 'FLDSTRENG',
+                              'FSVERSION', 'IMAGEUID', 'Ventricles', 'Hippocampus', 'WholeBrain', 'Entorhinal',
+                              'Fusiform', 'MidTemp', 'ICV', 'EXAMDATE_bl', 'CDRSB_bl', 'ADAS11_bl', 'ADAS13_bl',
+                              'ADASQ4_bl', 'MMSE_bl', 'RAVLT_immediate_bl', 'RAVLT_learning_bl', 'RAVLT_forgetting_bl',
+                              'RAVLT_perc_forgetting_bl', 'LDELTOTAL_BL', 'DIGITSCOR_bl', 'TRABSCOR_bl', 'FAQ_bl',
+                              'mPACCdigit_bl', 'mPACCtrailsB_bl', 'FLDSTRENG_bl', 'FSVERSION_bl', 'Ventricles_bl',
+                              'Hippocampus_bl', 'WholeBrain_bl', 'Entorhinal_bl', 'Fusiform_bl', 'MidTemp_bl', 'ICV_bl',
+                              'MOCA_bl', 'EcogPtMem_bl', 'EcogPtLang_bl', 'EcogPtVisspat_bl', 'EcogPtPlan_bl',
+                              'EcogPtOrgan_bl', 'EcogPtDivatt_bl', 'EcogPtTotal_bl', 'EcogSPMem_bl', 'EcogSPLang_bl',
+                              'EcogSPVisspat_bl', 'EcogSPPlan_bl', 'EcogSPOrgan_bl', 'EcogSPDivatt_bl', 'EcogSPTotal_bl',
+                              'ABETA_bl', 'TAU_bl', 'PTAU_bl', 'FDG_bl', 'PIB_bl', 'AV45_bl', 'Years_bl', 'Month_bl',
+                              'Month', 'M', 'update_stamp','DX_bl']
+
             # this field is the object id generated from MongoDB, we dont need it.
             labels = [label for label in db.getLabels() if label not in labelsToRemove]
 
@@ -30,41 +44,35 @@ class Merge:
             data = panda.DataFrame({label: db.getColumn(label) for label in labels})
             print("Download complete, treating data now...")
 
-            labelsToEval = ['AGE', 'PTEDUCAT', 'APOE4', 'FDG', 'PIB', 'AV45', 'CDRSB', 'ADAS11', 'ADAS13',
-                            'ADASQ4', 'MMSE', 'RAVLT_immediate', 'RAVLT_learning', 'RAVLT_forgetting', 'RAVLT_perc_forgetting',
+            labelsToEval = ['AGE', 'CDRSB', 'ADAS11', 'ADAS13', 'ADASQ4', 'MMSE', 'RAVLT_immediate', 'RAVLT_learning', 'RAVLT_forgetting', 'RAVLT_perc_forgetting',
                             'LDELTOTAL', 'DIGITSCOR', 'TRABSCOR', 'FAQ', 'MOCA', 'EcogPtMem', 'EcogPtLang', 'EcogPtVisspat',
                             'EcogPtPlan', 'EcogPtOrgan', 'EcogPtDivatt', 'EcogPtTotal', 'EcogSPMem', 'EcogSPLang', 'EcogSPVisspat',
                             'EcogSPPlan', 'EcogSPOrgan', 'EcogSPDivatt', 'EcogSPTotal', 'IMAGEUID',
                             'Ventricles', 'Hippocampus', 'WholeBrain', 'Entorhinal', 'Fusiform', 'MidTemp', 'ICV', 'mPACCdigit',
-                            'mPACCtrailsB', 'CDRSB_bl', 'ADAS11_bl', 'ADASQ4_bl', 'MMSE_bl', 'RAVLT_immediate_bl',
-                            'RAVLT_learning_bl', 'RAVLT_forgetting_bl', 'RAVLT_perc_forgetting_bl', 'LDELTOTAL_BL', 'DIGITSCOR_bl',
-                            'TRABSCOR_bl', 'FAQ_bl', 'mPACCdigit_bl', 'mPACCtrailsB_bl', 'Ventricles_bl',
-                            'Hippocampus_bl', 'WholeBrain_bl', 'Entorhinal_bl', 'Fusiform_bl', 'MidTemp_bl', 'ICV_bl', 'MOCA_bl',
-                            'EcogPtMem_bl', 'EcogPtLang_bl', 'EcogPtVisspat_bl', 'EcogPtPlan_bl', 'EcogPtOrgan_bl', 'EcogPtDivatt_bl',
-                            'EcogPtTotal_bl', 'EcogSPLang_bl', 'EcogSPVisspat_bl', 'EcogSPPlan_bl', 'EcogSPOrgan_bl', 'EcogSPDivatt_bl',
-                            'EcogSPTotal_bl', 'FDG_bl', 'PIB_bl', 'AV45_bl', 'Years_bl', 'Month_bl',
-                            'Month', 'M']
+                            'mPACCtrailsB', 'CDRSB_bl']
+
+            unusedLables = []
 
             # data = [[eval(x) if x != '' else x for x in data[label]] for label in labelsToEval]
 
             # WARNING POSSIBLE DATA CHANGE!
-            data['ABETA'] = [1700 if x == '>1700' else x for x in data['ABETA']]
-            data['ABETA'] = [200 if x == '<200' else x for x in data['ABETA']]
-            data['ABETA_bl'] = [1700 if x == '>1700' else x for x in data['ABETA_bl']]
-            data['ABETA_bl'] = [200 if x == '<200' else x for x in data['ABETA_bl']]
+            # data['ABETA'] = [1700 if x == '>1700' else x for x in data['ABETA']]
+            # data['ABETA'] = [200 if x == '<200' else x for x in data['ABETA']]
+            # data['ABETA_bl'] = [1700 if x == '>1700' else x for x in data['ABETA_bl']]
+            # data['ABETA_bl'] = [200 if x == '<200' else x for x in data['ABETA_bl']]
 
-            data['TAU'] = [1300 if x == '>1300' else x for x in data['TAU']]
-            data['TAU'] = [80 if x == '<80' else x for x in data['TAU']]
-            data['TAU_bl'] = [1300 if x == '>1300' else x for x in data['TAU_bl']]
-            data['TAU_bl'] = [80 if x == '<80' else x for x in data['TAU_bl']]
+            # data['TAU'] = [1300 if x == '>1300' else x for x in data['TAU']]
+            # data['TAU'] = [80 if x == '<80' else x for x in data['TAU']]
+            # data['TAU_bl'] = [1300 if x == '>1300' else x for x in data['TAU_bl']]
+            # data['TAU_bl'] = [80 if x == '<80' else x for x in data['TAU_bl']]
 
-            data['PTAU'] = [120 if x == '>120' else x for x in data['PTAU']]
-            data['PTAU'] = [8 if x == '<8' else x for x in data['PTAU']]
-            data['PTAU_bl'] = [120 if x == '>120' else x for x in data['PTAU_bl']]
-            data['PTAU_bl'] = [8 if x == '<8' else x for x in data['PTAU_bl']]
+            # data['PTAU'] = [120 if x == '>120' else x for x in data['PTAU']]
+            # data['PTAU'] = [8 if x == '<8' else x for x in data['PTAU']]
+            # data['PTAU_bl'] = [120 if x == '>120' else x for x in data['PTAU_bl']]
+            # data['PTAU_bl'] = [8 if x == '<8' else x for x in data['PTAU_bl']]
 
-            data['FLDSTRENG'] = [eval(x.strip(' Tesla MRI')) if ' Tesla MRI' in x else x for x in data['FLDSTRENG']]
-            data['FLDSTRENG_bl'] = [eval(x.strip(' Tesla MRI')) if ' Tesla MRI' in x else x for x in data['FLDSTRENG_bl']]
+            # data['FLDSTRENG'] = [eval(x.strip(' Tesla MRI')) if ' Tesla MRI' in x else x for x in data['FLDSTRENG']]
+            # data['FLDSTRENG_bl'] = [eval(x.strip(' Tesla MRI')) if ' Tesla MRI' in x else x for x in data['FLDSTRENG_bl']]
 
             # Labels:
             # CN        -> 0
@@ -79,28 +87,28 @@ class Merge:
             # EMCI      -> 2
             # AD        -> 3
             # SMC       -> 4
-            data['DX_bl'] = [0 if value == 'CN' else value for value in data['DX_bl']]
-            data['DX_bl'] = [1 if value == 'LMCI' else value for value in data['DX_bl']]
-            data['DX_bl'] = [2 if value == 'EMCI' else value for value in data['DX_bl']]
-            data['DX_bl'] = [3 if value == 'AD' else value for value in data['DX_bl']]
-            data['DX_bl'] = [4 if value == 'SMC' else value for value in data['DX_bl']]
+            # data['DX_bl'] = [0 if value == 'CN' else value for value in data['DX_bl']]
+            # data['DX_bl'] = [1 if value == 'LMCI' else value for value in data['DX_bl']]
+            # data['DX_bl'] = [2 if value == 'EMCI' else value for value in data['DX_bl']]
+            # data['DX_bl'] = [3 if value == 'AD' else value for value in data['DX_bl']]
+            # data['DX_bl'] = [4 if value == 'SMC' else value for value in data['DX_bl']]
 
             data['PTGENDER'] = [1 if x == 'Male' else 0 for x in data['PTGENDER']]
 
-            data['PTETHCAT'] = [0 if x == 'Not Hisp/Latino' else x for x in data['PTETHCAT']]
-            data['PTETHCAT'] = [1 if x == 'Hisp/Latino' else x for x in data['PTETHCAT']]
-            data['PTETHCAT'] = [2 if x == 'Unknown' else x for x in data['PTETHCAT']]
+            # data['PTETHCAT'] = [0 if x == 'Not Hisp/Latino' else x for x in data['PTETHCAT']]
+            # data['PTETHCAT'] = [1 if x == 'Hisp/Latino' else x for x in data['PTETHCAT']]
+            # data['PTETHCAT'] = [2 if x == 'Unknown' else x for x in data['PTETHCAT']]
 
-            data['PTMARRY'] = [0 if x == 'Unknown' else x for x in data['PTMARRY']]
-            data['PTMARRY'] = [1 if x == 'Married' else x for x in data['PTMARRY']]
-            data['PTMARRY'] = [2 if x == 'Widowed' else x for x in data['PTMARRY']]
-            data['PTMARRY'] = [3 if x == 'Divorced' else x for x in data['PTMARRY']]
-            data['PTMARRY'] = [4 if x == 'Never married' else x for x in data['PTMARRY']]
+            # data['PTMARRY'] = [0 if x == 'Unknown' else x for x in data['PTMARRY']]
+            # data['PTMARRY'] = [1 if x == 'Married' else x for x in data['PTMARRY']]
+            # data['PTMARRY'] = [2 if x == 'Widowed' else x for x in data['PTMARRY']]
+            # data['PTMARRY'] = [3 if x == 'Divorced' else x for x in data['PTMARRY']]
+            # data['PTMARRY'] = [4 if x == 'Never married' else x for x in data['PTMARRY']]
 
-            data['PTRACCAT'] = [0 if x == 'White' else 1 for x in data['PTRACCAT']]
+            # data['PTRACCAT'] = [0 if x == 'White' else 1 for x in data['PTRACCAT']]
             print("Saving file...")
 
-            data.to_pickle(path+'.pickle')
-            data.to_csv(path+'.csv')
+            data.to_pickle(self.path+'.pickle')
+            data.to_csv(self.path+'.csv')
 
         return data
